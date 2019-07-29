4fd940d6b46a97ec4faad9cddc97f518bdd1dd46
alfredtommy <alfredtommy@gmail.com>
2019-07-29 16:18:48 +0530

    Create cold_cluster_provisioner.sh

---
+++ cold_cluster_provisioner.sh
@@ -0,0 +1,44 @@
+#!/bin/bash	
+set -x
+
+create_template() {
+	gcloud compute instance-templates create $1 --custom-cpu="8" --custom-memory="61" --custom-extensions --image="cold-cluster-instance-recreated-image-new-v9" --boot-disk-type="pd-ssd" --boot-disk-size="30" --local-ssd interface=NVMe --local-ssd interface=NVMe --local-ssd interface=NVMe --local-ssd interface=NVMe --network="zapr-vpc-network" --subnet="private-1" --tags="allow-ssh" --preemptible --metadata-from-file startup-script="./cold-startup-script.sh"  --scopes="monitoring,pubsub,storage-rw" --metadata partition=$2 --region=asia-south1
+}
+
+create_instance_group(){
+	gcloud compute instance-groups managed create $1 --size=1 --template=$2 --base-instance-name=cold-instance --region=asia-south1 --health-check=router-hc
+}
+
+create_backend_service(){
+	gcloud compute backend-services create $1 --health-checks=router-hc --port-name=tcp8082 --protocol=HTTP --global
+}
+
+attach_backend() {
+	gcloud compute backend-services add-backend $1 --instance-group=$2 --instance-group-region=asia-south1 --global
+	gcloud compute instance-groups managed set-named-ports $2 --named-ports "tcp8082:8082" --region=asia-south1
+
+}
+
+create_path_rules() {
+  s=""
+  for ((i=1;i<=$1;i++)); do
+    s+=/cold-cluster/$i=cold-backend-$i,
+  done
+  s=${s%?};
+  gcloud compute url-maps add-path-matcher cold-http-lb --default-service test-backend --path-matcher-name path-matcher-1 --path-rules $s --new-hosts "*" --delete-orphaned-path-matcher
+}
+
+
+
+main(){
+	for ((i=1; i<=$1; i++)); do
+        create_template cold-temp-$i $i
+        create_instance_group cold-group-$i cold-temp-$i
+        create_backend_service cold-backend-$i
+        attach_backend cold-backend-$i cold-group-$i
+    done   
+}
+
+read -p "Enter the number of templates you want to create: " count
+main $count
+create_path_rules $count
