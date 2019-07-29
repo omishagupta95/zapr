87796c551fc87afab9d2e6dda332e5ec40295328
alfredtommy <alfredtommy@gmail.com>
2019-07-29 16:27:27 +0530

    Update hot-startup-script.sh

--- hot-startup-script.sh
+++ hot-startup-script.sh
@@ -1,13 +1,11 @@
 #!/bin/bash
 set -x
-
 get_data() {
     metadata=$(curl http://169.254.169.254/0.1/meta-data/attributes/partition)
-    gsutil -m cp gs://zapr_bucket/hotcluster/$kyotoPartition/hotcluster.txt /opt/kyoto/hotcluster.txt
-    gsutil -m cp gs://zapr_bucket/hotcluster/$kyotoPartition/matcher.kch  /opt/kyoto/matcher.kch
-    gsutil -m cp gs://zapr_bucket/hotcluster/$kyotoPartition/prefilter.kch  /opt/kyoto/prefilter.kch
+    gsutil -m cp gs://zapr_bucket/hotcluster/$metadata/hotcluster.txt /opt/kyoto/hotcluster.txt
+    gsutil -m -o GSUtil:parallel_composite_upload_threshold=150M cp gs://zapr_bucket/hotcluster/$metadata/matcher.kch  /opt/kyoto/matcher.kch
+    gsutil -m -o GSUtil:parallel_composite_upload_threshold=150M cp gs://zapr_bucket/hotcluster/$metadata/prefilter.kch  /opt/kyoto/prefilter.kch
 }
-
 main() {
     sudo mount -t tmpfs -o size=35G tmpfs /opt/kyoto
     /opt/zapr/prod-active-song-revealer/scripts/kyotoFix.sh
