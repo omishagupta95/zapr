f6d8162cb1ea5f7dd1eda1316de5e8a103930660
alfredtommy <alfredtommy@gmail.com>
2019-07-30 18:59:26 +0530

    Update cold-startup-script.sh

--- cold-startup-script.sh
+++ cold-startup-script.sh
@@ -2,15 +2,9 @@
 set -x
 
 get_data() {
-  metadata=$(curl http://169.254.169.254/0.1/meta-data/attributes/partition)
-  gsutil -m cp gs://zapr_bucket/Coldcluster/matcherreduced-$metadata.kch  /opt/bin/matcher.kch
-  gsutil -m cp gs://zapr_bucket/Coldcluster/prefilter-$metadata.kch /opt/bin/prefilter.kch
-}
-
-get_data() {
     metadata=$(curl http://169.254.169.254/0.1/meta-data/attributes/partition)
-    gsutil -m -o GSUtil:parallel_composite_upload_threshold=150M cp gs://zapr_bucket/Coldcluster/matcherreduced-$metadata.kch  /opt/bin/matcher.kch
-    gsutil -m -o GSUtil:parallel_composite_upload_threshold=150M cp gs://zapr_bucket/Coldcluster/prefilter-$metadata.kch /opt/bin/prefilter.kch
+    gsutil -m -o GSUtil:parallel_composite_upload_threshold=150M cp gs://zapr_bucket/Coldcluster/matcherreduced-$metadata.kch  /mnt/md0/matcher.kch
+    gsutil -m -o GSUtil:parallel_composite_upload_threshold=150M cp gs://zapr_bucket/Coldcluster/prefilter-$metadata.kch /mnt/md0/prefilter.kch
 }
 
 copy_data() {
