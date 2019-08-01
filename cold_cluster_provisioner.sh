69e11ec37b4bec65f751383721d73203353b6fe8
Omisha Gupta <omishagupta95@gmail.com>
2019-08-01 16:47:21 +0530

    Update cold_cluster_provisioner.sh

--- cold_cluster_provisioner.sh
+++ cold_cluster_provisioner.sh
@@ -2,7 +2,7 @@
 set -x
 
 create_template() {
-	gcloud compute instance-templates create $1 --custom-cpu="8" --custom-memory="61" --custom-extensions --image="cold-cluster-instance-recreated-image-new-v10" --boot-disk-type="pd-ssd" --boot-disk-size="30" --local-ssd interface=NVMe --local-ssd interface=NVMe --local-ssd interface=NVMe --local-ssd interface=NVMe --network="zapr-vpc-network" --subnet="private-1" --tags="allow-ssh" --preemptible --metadata-from-file startup-script="./cold-startup-script.sh"  --scopes="monitoring,pubsub,storage-rw" --metadata partition=$2 --region=asia-south1
+	gcloud compute instance-templates create $1 --custom-cpu="8" --custom-memory="61" --custom-extensions --image="image-for-cold-cluster" --boot-disk-type="pd-ssd" --boot-disk-size="30" --local-ssd interface=NVMe --local-ssd interface=NVMe --local-ssd interface=NVMe --local-ssd interface=NVMe --network="zapr-vpc-network" --subnet="private-1" --tags="allow-ssh" --preemptible --metadata-from-file startup-script="./cold-startup-script.sh"  --scopes="monitoring,pubsub,storage-rw" --metadata partition=$2 --region=asia-south1
 }
 
 create_instance_group(){
