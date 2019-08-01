e1dc8ab2f73c15ca6ddd2dc4fa53ac2aec77b4ef
alfredtommy <alfredtommy@gmail.com>
2019-08-01 19:55:43 +0530

    Update cold-startup-script.sh

--- cold-startup-script.sh
+++ cold-startup-script.sh
@@ -5,14 +5,19 @@ get_data() {
     metadata=$(curl http://169.254.169.254/0.1/meta-data/attributes/partition)
     gsutil -m -o GSUtil:parallel_composite_upload_threshold=150M cp gs://zapr_bucket/Coldcluster/$metadata/matcherreduced-$metadata.kch  /mnt/md0/matcher.kch
     gsutil -m -o GSUtil:parallel_composite_upload_threshold=150M cp gs://zapr_bucket/Coldcluster/$metadata/prefilter-$metadata.kch /mnt/md0/prefilter.kch
+}
+
+update_nginx() {
+    metadata=$(curl http://169.254.169.254/0.1/meta-data/attributes/partition)
     sudo sed -i "s/metadata/$metadata/" /etc/nginx/sites-enabled/default
     sudo service nginx restart
-}
+}    
 
 copy_data() {
   /opt/zapr/prod-active-song-revealer/scripts/kyotoFix.sh
   .//root/script/md0.sh
   get_data
+  update_nginx
   mkdir -p /opt/zapr/prod-active-song-revealer/logs
   ansible-playbook /opt/zapr/prod-active-song-revealer/deploy/prod/active/hot/song-revealer.yml | tee /opt/zapr/prod-active-song-revealer/logs/deploy.log
 }
