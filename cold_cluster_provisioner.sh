98f28d2d89bbb38509faa32597921789489bec21
alfredtommy <alfredtommy@gmail.com>
2019-08-05 20:44:48 +0530

    Update cold_cluster_provisioner.sh

--- cold_cluster_provisioner.sh
+++ cold_cluster_provisioner.sh
@@ -22,7 +22,7 @@ attach_backend() {
 create_path_rules() {
   s=""
   for ((i=1;i<=$1;i++)); do
-    s+=/coldcluster/$i=cold-backend-$i,
+    s+=/coldcluster/$i/*=cold-backend-$i,
   done
   s=${s%?}; // To remove the last comma
   gcloud compute url-maps add-path-matcher cold-http-lb --default-service test-backend --path-matcher-name path-matcher-1 --path-rules $s --new-hosts "*" --delete-orphaned-path-matcher
