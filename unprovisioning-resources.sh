a46e4ec5ab12097211f6fa5ba527efecdb4a6a03
omishagupta95 <omishagupta95@gmail.com>
2019-08-06 13:02:01 +0530

    unprovisioning resources for hot and cold clusters.

--- unprovisioning-resources.sh
+++ unprovisioning-resources.sh
@@ -2,34 +2,36 @@
 set -x
 
 delete_template() {
-	gcloud compute instance-templates delete $1
+	gcloud compute instance-templates delete $1 -q
 }
 
 delete_instance_group() {
-	gcloud compute instance-groups managed delete $1 --region=asia-south1
+	gcloud compute instance-groups managed delete $1 --region=asia-south1 -q
 }
 
 delete_backend_service() {
-	gcloud compute backend-services delete $1 --global
+	gcloud compute backend-services delete $1 --global -q
 }
 
 delete_path_matcher() {
-  gcloud compute url-maps remove-path-matcher test-http-lb --path-matcher-name path-matcher-1
+  gcloud compute url-maps remove-path-matcher $1-http-lb --path-matcher-name path-matcher-1 -q 
 }
 
 attach_dummy_backend() {
-  gcloud compute backend-services add-backend $1 --instance-group=$2 --instance-group-region=us-east1 --global
+  gcloud compute backend-services add-backend $1 --instance-group=hot-cluster-unmanaged-instance-group --instance-group-region=asia-south1 --global -q
 }
 
 main(){
 for ((i=1; i<=$1; i++)); do
-	attach_dummy_backend mukesh-test-backend mukesh-test-instance-group-tomcat
-	delete_path_matcher
-	delete_backend_service hot-backend-$i
-	delete_instance_group hot-group-$i
-    delete_template hot-temp-$i
+	attach_dummy_backend test-backend $2-cluster-unmanaged-instance-group
+	delete_backend_service $2-backend-$i
+	delete_instance_group $2-group-$i
+    	delete_template $2-temp-$i
     done
 }
 
+read -p "which resources you want deprovision, hot or cold (case senstive)" type
 read -p "Enter the number of templates you want to delete: " count
-main $count
+delete_path_matcher $type
+main $count $type
+
