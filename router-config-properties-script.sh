f4dfc4650592c9effc3ef143fc76f68aa46c8a62
alfredtommy <alfredtommy@gmail.com>
2019-09-27 17:06:27 +0530

    Update router-config-properties-script.sh

--- router-config-properties-script.sh
+++ router-config-properties-script.sh
@@ -11,14 +11,30 @@ echo $s;
 echo "-----------------------------------------------------"
 
 t=""
-for ((i=1;i<=100;i++)); do
+for ((i=1;i<=50;i++)); do
   t+=http://35.190.22.73:80/coldcluster/$i/match_progressive,
 done
 t=${t%?};
-echo "--------------THIS IS FOR COLDCLUSTER-----------------"
+echo "--------------THIS IS FOR COLDCLUSTER-1-----------------"
 echo $t;
 echo "------------------------------------------------------"
-
+echo "======================================================"
+u=""
+for ((i=51;i<=100;i++)); do
+  u+=http://35.244.147.101:80/coldcluster/$i/match_progressive,
+done
+u=${u%?};
+echo "--------------THIS IS FOR COLDCLUSTER-2-----------------"
+echo $u;
+echo "========================================================"
+v=""
+for ((i=101;i<=103;i++)); do
+  v+=http://35.201.104.120:80/coldcluster/$i/match_progressive,
+done
+v=${v%?};
+echo "--------------THIS IS FOR COLDCLUSTER-3-----------------"
+echo $v;
+echo "========================================================"
 }
 
 main
