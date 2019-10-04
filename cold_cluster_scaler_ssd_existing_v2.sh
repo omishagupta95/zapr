ebac147f5a0db51ac6ec79d40f1bf751f81a9fc0
alfredtommy <alfredtommy@gmail.com>
2019-10-04 19:31:08 +0530

    Update cold_cluster_scaler_ssd_existing_v2.sh

--- cold_cluster_scaler_ssd_existing_v2.sh
+++ cold_cluster_scaler_ssd_existing_v2.sh
@@ -12,7 +12,7 @@ then
      if [[ $3 -eq 1 ]] || [[ $3 -eq 13 ]] || [[ $3 -eq 14 ]] || [[ $3 -eq 17 ]] || [[ $3 -eq 18 ]] || [[ $3 -eq 19 ]] || [[ $3 -eq 20 ]] || [[ $3 -eq 23 ]] || [[ $3 -eq 25 ]] || [[ $3 -eq 26 ]] || [[ $3 -eq 27 ]] || [[ $3 -eq 3 ]] || [[ $3 -eq 32 ]] || [[ $3 -eq 33 ]] || [[ $3 -eq 34 ]]
      then
         gcloud compute instance-groups managed create $1 --size=1 --template=$2 --base-instance-name=cold-instance --zone=asia-south1-b --health-check=router-hc --initial-delay 2700
-     elif [[ $3 -eq 2 ]] || [[ $3 -eq 4 ]] || [[ $3 -eq 5 ]] || [[ $3 -eq 7 ]] || [[ $3 -eq 8 ]] || [[ $3 -eq 9 ]] || [[ $3 -eq 11 ]] || [[ $3 -eq 15 ]] || [[ $3 -eq 12 ]] || [[ $3 -eq 24 ]] || [[ $3 -eq 29 ]] || [[ $3 -eq 30 ]] 
+     elif [[ $3 -eq 2 ]] || [[ $3 -eq 4 ]] || [[ $3 -eq 5 ]] || [[ $3 -eq 7 ]] || [[ $3 -eq 8 ]] || [[ $3 -eq 9 ]] || [[ $3 -eq 11 ]] || [[ $3 -eq 15 ]] || [[ $3 -eq 12 ]] || [[ $3 -eq 22 ]] || [[ $3 -eq 24 ]] || [[ $3 -eq 29 ]] || [[ $3 -eq 30 ]] 
      then
         gcloud compute instance-groups managed create $1 --size=1 --template=$2 --base-instance-name=cold-instance --zone=asia-south1-a --health-check=router-hc --initial-delay 2700
      elif [[ $3 -eq 6 ]] || [[ $3 -eq 10 ]] || [[ $3 -eq 12 ]] || [[ $3 -eq 16 ]] || [[ $3 -eq 21 ]] || [[ $3 -eq 28 ]] || [[ $3 -eq 31 ]] 
@@ -66,7 +66,7 @@ fi
 }
 
 attach_backend() {
-  if [[ $3 -eq 2 ]] || [[ $3 -eq 4 ]] || [[ $3 -eq 5 ]] || [[ $3 -eq 7 ]] || [[ $3 -eq 8 ]] || [[ $3 -eq 9 ]] || [[ $3 -eq 11 ]] || [[ $3 -eq 15 ]] || [[ $3 -eq 12 ]] || [[ $3 -eq 24 ]] || [[ $3 -eq 29 ]] || [[ $3 -eq 30 ]] || [[ $3 -eq 39 ]] || [[ $3 -eq 40 ]] || [[ $3 -eq 41 ]] || [[ $3 -eq 43 ]] || [[ $3 -eq 44 ]] || [[ $3 -eq 51 ]] || [[ $3 -eq 52 ]] || [[ $3 -eq 53 ]] || [[ $3 -eq 56 ]] || [[ $3 -eq 63 ]] || [[ $3 -eq 65 ]] || [[ $3 -eq 66 ]] || [[ $3 -eq 67 ]] || [[ $3 -eq 69 ]] || [[ $3 -eq 72 ]] || [[ $3 -eq 75 ]] || [[ $3 -eq 78 ]] || [[ $3 -eq 80 ]] || [[ $3 -eq 81 ]] || [[ $3 -eq 82 ]] || [[ $3 -eq 86 ]] || [[ $3 -eq 89 ]] || [[ $3 -eq 94 ]] || [[ $3 -eq 96 ]] || [[ $3 -eq 102 ]] 
+  if [[ $3 -eq 2 ]] || [[ $3 -eq 4 ]] || [[ $3 -eq 5 ]] || [[ $3 -eq 7 ]] || [[ $3 -eq 8 ]] || [[ $3 -eq 9 ]] || [[ $3 -eq 11 ]] || [[ $3 -eq 15 ]] || [[ $3 -eq 12 ]] || [[ $3 -eq 22 ]] || [[ $3 -eq 24 ]] || [[ $3 -eq 29 ]] || [[ $3 -eq 30 ]] || [[ $3 -eq 39 ]] || [[ $3 -eq 40 ]] || [[ $3 -eq 41 ]] || [[ $3 -eq 43 ]] || [[ $3 -eq 44 ]] || [[ $3 -eq 51 ]] || [[ $3 -eq 52 ]] || [[ $3 -eq 53 ]] || [[ $3 -eq 56 ]] || [[ $3 -eq 63 ]] || [[ $3 -eq 65 ]] || [[ $3 -eq 66 ]] || [[ $3 -eq 67 ]] || [[ $3 -eq 69 ]] || [[ $3 -eq 72 ]] || [[ $3 -eq 75 ]] || [[ $3 -eq 78 ]] || [[ $3 -eq 80 ]] || [[ $3 -eq 81 ]] || [[ $3 -eq 82 ]] || [[ $3 -eq 86 ]] || [[ $3 -eq 89 ]] || [[ $3 -eq 94 ]] || [[ $3 -eq 96 ]] || [[ $3 -eq 102 ]] 
   then
     gcloud compute backend-services add-backend $1 --instance-group=$2 --instance-group-zone=asia-south1-a --global
     gcloud compute instance-groups managed set-named-ports $2 --named-ports "http:80" --zone=asia-south1-a
