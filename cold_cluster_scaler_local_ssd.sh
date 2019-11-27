970a96d129be1d28266e9e04fc7988d7a6a89ad5
alfredtommy <alfredtommy@gmail.com>
2019-11-27 23:10:47 +0530

    Update cold_cluster_scaler_local_ssd.sh

--- cold_cluster_scaler_local_ssd.sh
+++ cold_cluster_scaler_local_ssd.sh
@@ -4,13 +4,13 @@ set -x
 # THIS SCRIPT IS FOR PROVISIONING COLD CLUSTER VMs with 1 local SSD
 # THIS SCRIPT DOESN'T INTENDS TO CREATE LOAD BALANCER, IT ASSUMES THAT LOAD BALANCER WITH THE SIMILAR NAME ALREADY EXISTS
 
-create_template_existing_disk() {
-        gcloud compute instance-templates create $1 --custom-cpu="8" --custom-memory="61" --custom-extensions --image="cold-cluster-instance-recreated-image-new-v18" --boot-disk-type="pd-ssd" --boot-disk-size="50" --disk=name=cold-disk-00$i,mode=ro --network="zapr-vpc-network" --subnet="private-1" --tags="zapr-polestar-revealer" $3 --metadata-from-file startup-script="./cold-startup-script-existing-disk.sh"  --scopes="monitoring,pubsub,storage-rw" --metadata partition=$2 --region=asia-south1
+#create_template_existing_disk() {
+#        gcloud compute instance-templates create $1 --custom-cpu="8" --custom-memory="61" --custom-extensions --image="cold-cluster-instance-recreated-image-new-v18" --boot-disk-type="pd-ssd" --boot-disk-size="50" --disk=name=cold-disk-00$i,mode=ro --network="zapr-vpc-network" --subnet="private-1" --tags="zapr-polestar-revealer" $3 --metadata-from-file startup-script="./cold-startup-script-existing-disk.sh"  --scopes="monitoring,pubsub,storage-rw" --metadata partition=$2 --region=asia-south1
 #        --disk=name=cold-disk-$i  use this instead of create-disk flag once all disks have been created
-}
+#}
 
 create_template_new_disk() {
-        gcloud compute instance-templates create $1 --custom-cpu="8" --custom-memory="61" --custom-extensions --image="cold-cluster-instance-recreated-image-new-v18" --boot-disk-type="pd-ssd" --boot-disk-size="50" --local-ssd interface=NVMe --network="zapr-vpc-network" --subnet="private-1" --tags="zapr-polestar-revealer" $3 --metadata-from-file startup-script="./cold_startup_script.sh",partition=$2  --scopes="monitoring,pubsub,storage-rw"  --region=asia-south1
+        gcloud compute instance-templates create $1 --custom-cpu="8" --custom-memory="61" --custom-extensions --image="cold-cluster-instance-recreated-image-new-v18" --boot-disk-type="pd-ssd" --boot-disk-size="50" --local-ssd interface=NVMe --network="zapr-vpc-network" --subnet="private-1" --tags="zapr-polestar-revealer" $3 --metadata startup-script-url="gs://zapr_bucket/startup-script/cold_startup.sh",partition=$2  --scopes="monitoring,pubsub,storage-rw"  --region=asia-south1
 #        --disk=name=cold-disk-$i  use this instead of create-disk flag once all disks have been created
 }
 
@@ -56,25 +56,20 @@ create_path_rules() {
 }
 
 execute(){
-      if [ "$option" == "true" ] || [ "$option" == "True" ]
-      then
-        echo "Caution: This creates new PD-SSD of size 500 gb"
-        create_template_new_disk cold-temp-$1 $1 $2
-      else
-        echo "Caution: This attaches existing PD-SSD of size 500 gb"
-        create_template_existing_disk cold-temp-$1 $1 $2
-      fi
       if (( $1 % 3 == 1 ))
       then
+        create_template_new_disk cold-temp-$1 $1 $2
         create_instance_group cold-group-$1 cold-temp-$1 $1 asia-south1-a
         create_backend_service cold-backend-$1 $1
         attach_backend cold-backend-$1 cold-group-$1 asia-south1-a
       elif (( $1 % 3 == 2 ))
       then
+        create_template_new_disk cold-temp-$1 $1 $2
         create_instance_group cold-group-$1 cold-temp-$1 $1 asia-south1-b
         create_backend_service cold-backend-$1 $1
         attach_backend cold-backend-$1 cold-group-$1 asia-south1-b
       else (( $1 % 3 == 0 ))
+        create_template_new_disk cold-temp-$1 $1 $2
         create_instance_group cold-group-$1 cold-temp-$1 $1 asia-south1-c
         create_backend_service cold-backend-$1 $1
         attach_backend cold-backend-$1 cold-group-$1 asia-south1-c
@@ -90,7 +85,6 @@ main() {
 read -p "This script is for scaling of cold instance groups. If you have 3 IGs, and you want to scale up to 10. Set start value as 4, and end value as 10. Please enter the start value: " start
 read -p "Enter the total number of cold instance groups you want to create, i.e, the end value: " end
 read -p "Choose preemptible(--preemptible) or non-preemptible(press spacebar and enter){case sensitive}: " type
-read -p "Are the disks new? (True/False): " option
 main $start $end
 sleep 250s
 if [ $end -lt 50 ]
